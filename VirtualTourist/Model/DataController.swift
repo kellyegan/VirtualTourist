df1046e47e6d49ed1fc6fa9f1c3d8dcbd2e3fcc4
kellyegan <kelly.egan@gmail.com>
2018-02-16 09:51:26 -0500

    Add DataController

---
+++ VirtualTourist/Model/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by Kelly Egan on 2/16/18.
+//  Copyright © 2018 Kelly Egan. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer:NSPersistentContainer
+    
+    init(modelName:String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    // MARK: - Helper functions
+    var viewContext:NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+}
