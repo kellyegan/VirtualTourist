549158ef9bdbc8727df6a297913e2cbaef549ce7
kellyegan <kelly.egan@gmail.com>
2018-02-25 16:33:24 -0500

    Add stubs for FlickrApi class and constants

---
+++ VirtualTourist/Model/Flickr/FlickrConstants.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrConstants.swift
+//  VirtualTourist
+//
+//  Created by Kelly Egan on 2/25/18.
+//  Copyright © 2018 Kelly Egan. All rights reserved.
+//
+
+import Foundation
+
+extension FlickrClient {
+    
+}
