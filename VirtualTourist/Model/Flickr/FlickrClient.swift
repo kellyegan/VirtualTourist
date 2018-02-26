d5c499a4e81528c3d02b9a788319dace9655cc2b
kellyegan <kelly.egan@gmail.com>
2018-02-26 15:24:40 -0500

    Create FlickrClient class methods for constructing REST request and searching for photos by location

--- VirtualTourist/Model/Flickr/FlickrClient.swift
+++ VirtualTourist/Model/Flickr/FlickrClient.swift
@@ -10,4 +10,51 @@ import Foundation
 
 class FlickrClient: NSObject {
     
+    /**
+     Create a request for an image search at a specific coordinate and given radius. Searches only photos in the last year.
+     
+     - parameter latitude: Latitude of coordinate in degrees
+     - parameter longitude: Longitude of coordinate in degrees
+     - parameter radius: Radius around coordinate to search in kilometers
+     
+     */
+    func searchPhotosByLocation( latitude: Double, longitude: Double, radius: Double ) {
+        let yearInSeconds:Double = 60 * 60 * 24 * 365
+        let aYearAgo = Date() - yearInSeconds
+        
+        let methodParameters = [
+            ParameterKeys.Method: ParameterValues.SearchPhotosMethod,
+            ParameterKeys.Latitude: latitude,
+            ParameterKeys.Longitude: longitude,
+            ParameterKeys.SearchRadius: radius,
+            ParameterKeys.MinimumDateTaken: aYearAgo.timeIntervalSince1970
+            ] as [String : Any]
+        
+        if let url = constructURL(parameters: methodParameters) {
+            print(url)
+        } else {
+            print("Something's borked.")
+        }
+    }
+    
+    private func constructURL(parameters: [String:Any]) -> URL? {
+        var components = URLComponents()
+        components.scheme = Flickr.scheme
+        components.host =  Flickr.host
+        components.path = Flickr.basePath
+        
+        var queryItems = [URLQueryItem]()
+        
+        //Standard parameters for every request: API Key, Response Format, Disable JSON callback
+        queryItems.append(URLQueryItem(name: ParameterKeys.APIKey, value: API.Key))
+        queryItems.append(URLQueryItem(name: ParameterKeys.ResponseFormat, value: ParameterValues.ResponseFormat))
+        queryItems.append(URLQueryItem(name: ParameterKeys.NoJSONCallback , value: ParameterValues.DisableJSONCallback))
+            
+        for (key, value) in parameters {
+            queryItems.append(URLQueryItem(name: key, value: "\(value)"))
+        }
+        components.queryItems = queryItems
+        
+        return components.url
+    }
 }
--- VirtualTourist/Model/Flickr/FlickrConstants.swift
+++ VirtualTourist/Model/Flickr/FlickrConstants.swift
@@ -9,5 +9,46 @@
 import Foundation
 
 extension FlickrClient {
+    // MARK: Flickr
+    struct Flickr {
+        static let scheme = "https"
+        static let host = "api.flickr.com"
+        static let basePath = "/services/rest"
+    }
     
+    // MARK: Flickr Parameter Keys
+    struct ParameterKeys {
+        static let Method = "method"
+        static let APIKey = "api_key"
+        static let Extras = "extras"
+        static let ResponseFormat = "format"
+        static let NoJSONCallback = "nojsoncallback"
+        static let Longitude = "lon"
+        static let Latitude = "lat"
+        static let SearchRadius = "radius"
+        static let IsCommons = "is_commons"
+        static let MinimumDateTaken = "min_taken_date"
+    }
+    
+    // MARK: Flickr Parameter Values
+    struct ParameterValues {
+        static let ResponseFormat = "json"
+        static let DisableJSONCallback = "1" /* 1 means "yes" */
+        static let SearchPhotosMethod = "flickr.photos.search"
+        static let MediumURL = "url_m"
+    }
+    
+    // MARK: Flickr Response Keys
+    struct ResponseKeys {
+        static let Status = "stat"
+        static let Photos = "photos"
+        static let Photo = "photo"
+        static let Title = "title"
+        static let MediumURL = "url_m"
+    }
+    
+    // MARK: Flickr Response Values
+    struct ResponseValues {
+        static let OKStatus = "ok"
+    }
 }
