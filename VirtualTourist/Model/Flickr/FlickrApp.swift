6cc0176293dd0058fd9ce190d764beaefce77ebf
kellyegan <kelly.egan@gmail.com>
2018-03-12 09:37:01 -0400

    Add FlickrApp.swift

---
+++ VirtualTourist/Model/Flickr/FlickrApp.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrApp.swift
+//  VirtualTourist
+//
+//  Created by Kelly Egan on 2/25/18.
+//  Copyright © 2018 Kelly Egan. All rights reserved.
+//
+
+import Foundation
+
+extension FlickrClient {
+    struct API {
+        static let Key: String = "API_KEY"
+        static let Secret: String = "API_SECRET"
+    }
+}
